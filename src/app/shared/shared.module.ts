253b5fbae03f471b87746a16a2c15fd72f14971f
Swaniket <swaniketchowdhury@gmail.com>
2022-02-28 10:04:46 +0530

    Implemented modal switch tab functionality & bug fixes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+// import { ModalService } from '../services/modal.service';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule
   ],
+  // We need to manually export components from a module
   exports: [
-    ModalComponent
-  ]
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
+  ],
 })
 export class SharedModule { }

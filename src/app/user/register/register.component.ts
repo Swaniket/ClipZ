42681118ecdd5a19c22980ae9136473435f4345c
Swaniket <swaniketchowdhury@gmail.com>
2022-03-15 15:35:59 +0530

    Added Async Validators for email

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
 
   inSubmission = false;
 
@@ -18,7 +20,11 @@ export class RegisterComponent {
   alertColor = 'blue';
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new FormControl('', [
     Validators.required,
     Validators.min(13),
@@ -35,14 +41,17 @@ export class RegisterComponent {
     Validators.maxLength(10),
   ]);
 
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
     this.showAlert = true;
---
+++ src/app/user/validators/email-taken.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  AsyncValidator,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmailTaken implements AsyncValidator {
+  constructor(private auth: AngularFireAuth) {}
+
+  validate = (control: AbstractControl): Promise<ValidationErrors | null> => {
+    return this.auth
+      .fetchSignInMethodsForEmail(control.value)
+      .then((response) => (response.length ? { emailTaken: true } : null));
+  };
+}

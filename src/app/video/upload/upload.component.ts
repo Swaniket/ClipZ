bf61907e25114f6d10f8be07dbb1d63665cfdf48
Swaniket <swaniketchowdhury@gmail.com>
2022-03-20 13:08:13 +0530

    Added reactive form to upload module

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
-  isDragOver = false
+  isDragOver = false;
+  nextStep = false;
+  file: File | null = null;
 
-  constructor() { }
+  // Form
+  title = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
-  ngOnInit(): void {
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // Read & process the uploaded file
+  storeFile(event: Event) {
+    this.isDragOver = false;
+
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    // Validation of File Type
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+    this.nextStep = true;
   }
 
+  // Handle form Submission
+  uploadFile() {
+    console.log('File Uploaded!');
+  }
 }
